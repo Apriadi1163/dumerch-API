57bade0311d942ddc635147a33c2e9ed79dfbb9f
Your Name <apriadi1163@gmail.com>
2022-08-19 06:46:17 +0700

    update product backend

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -177,15 +177,21 @@ exports.getProduct = async (req, res) => {
 exports.updateProduct = async (req, res) => {
   const { id } = req.params;
   try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'Upload',
+      use_filename: true,
+      unique_filename: false,
+    });
+
     const data = req.body;
-    if (req.file) {
-      data.image = req.file.filename;
-    }
+    // if (req.file) {
+    //   data.image = req.file.filename;
+    // }
     console.log(data);
     let updateProduct = await product.update(
       {
         ...data,
-        // image: req.file.filename,
+        image: result.public_id,
         idUser: req.user.id,
       },
       { where: { id } }
@@ -195,7 +201,7 @@ exports.updateProduct = async (req, res) => {
 
     updateProduct = {
       ...updateProduct,
-      image: process.env.PATH_FILE + updateProduct.image,
+      image: process.env.PATH_FILE + result.public_id,
     };
 
     res.status(200).send({
